15068985ea5e7788ee73cdc2ef07043ecc86d4ef
Pasha Bibko <156938226+PashaBibko@users.noreply.github.com>
2025-06-20 11:58:05 +0100

    Added player prefab
    Also organised files

---
+++ Assets/Scripts/Wrappers/SyncedTransform.cs
@@ -0,0 +1,9 @@
+using Unity.Netcode.Components;
+using UnityEngine;
+
+// Wrapper to allow the client to be able to control the positions of transforms //
+[DisallowMultipleComponent] public class SyncedTransform : NetworkTransform
+{
+    // Tells the script it is a NON authoritive server which allows free changing of transforms //
+    protected override bool OnIsServerAuthoritative() => false;
+}
